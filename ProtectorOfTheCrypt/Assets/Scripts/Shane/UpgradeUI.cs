b71345c318bb572ee298deeac7b8a9a0126dbfad
shanepearson62 <108286163+shanepearson62@users.noreply.github.com>
2023-10-11 18:21:13 -0400

    Update to Upgrade System/UI/Prefabs
    Upgrades now work in game

--- ProtectorOfTheCrypt/Assets/Scripts/Shane/UpgradeUI.cs
+++ ProtectorOfTheCrypt/Assets/Scripts/Shane/UpgradeUI.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class UpgradeUI : MonoBehaviour
 {
     public GameObject UpUI;
+    public TowerUpgradeHandler TowerUpRef;
 
     // Appear() is to make the Upgrade Ui Appear. Call this function when the player WANTS to see the upgrade UI
     public void Appear()
@@ -16,5 +17,24 @@ public class UpgradeUI : MonoBehaviour
     public void Hide()
     {
         UpUI.SetActive(false);
+    }
+
+    // Start of Button Functions
+    public void DamageButton()
+    {
+        // Call UpgradeDamageAdd(upgrade amount, cost);
+        TowerUpRef.UpgradeDamageAdd(2, 25);
+    }
+
+    public void RangeButton()
+    {
+        // Call UpgradeRangeAdd(upgrade amount, cost);
+        TowerUpRef.UpgradeRangeAdd(2, 15);
+    }
+
+    public void FireRateButton()
+    {
+        // Call UpgradeFireRateAdd(upgrade amount, cost);
+        TowerUpRef.UpgradeFireRateSubtract(2, 20);
     }
 }
