5dfec69cf2056664fe5a998ea62d7ef1f11ecfee
shanepearson62 <108286163+shanepearson62@users.noreply.github.com>
2023-10-25 19:27:59 -0400

    Upgrade Particle Effect In Game
    title

--- ProtectorOfTheCrypt/Assets/Scripts/Shane/UpgradeUI.cs
+++ ProtectorOfTheCrypt/Assets/Scripts/Shane/UpgradeUI.cs
@@ -6,6 +6,7 @@ public class UpgradeUI : MonoBehaviour
 {
     public GameObject UpUI;
     public TowerUpgradeHandler TowerUpRef;
+    public ParticleSystem upgradePS;
 
     // Appear() is to make the Upgrade Ui Appear. Call this function when the player WANTS to see the upgrade UI
     public void Appear()
@@ -24,17 +25,20 @@ public class UpgradeUI : MonoBehaviour
     {
         // Call UpgradeDamageAdd(upgrade amount, cost);
         TowerUpRef.UpgradeDamageAdd(2, 25);
+        upgradePS.Play();
     }
 
     public void RangeButton()
     {
         // Call UpgradeRangeAdd(upgrade amount, cost);
         TowerUpRef.UpgradeRangeAdd(2, 15);
+        upgradePS.Play();
     }
 
     public void FireRateButton()
     {
         // Call UpgradeFireRateAdd(upgrade amount, cost);
         TowerUpRef.UpgradeFireRateSubtract(2, 20);
+        upgradePS.Play();
     }
 }
