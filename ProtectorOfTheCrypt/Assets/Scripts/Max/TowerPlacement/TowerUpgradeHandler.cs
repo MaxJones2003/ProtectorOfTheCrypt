019e5152fd3cd428ea7a0fc5f5bbc1b6d132d1bb
Tyler <gordontwatts@gmail.com>
2023-11-08 17:46:53 -0500

    Tier Upgrade System, and Scout Enemy added to Endless Mode and Level 2

--- ProtectorOfTheCrypt/Assets/Scripts/Max/TowerPlacement/TowerUpgradeHandler.cs
+++ ProtectorOfTheCrypt/Assets/Scripts/Max/TowerPlacement/TowerUpgradeHandler.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class TowerUpgradeHandler : MonoBehaviour
 {
-    [SerializeField] private GameObject upgradeUI;
+    [SerializeField] private GameObject upgradeUITier2;
+    [SerializeField] private GameObject upgradeUITier3;
+    [SerializeField] private GameObject upgradeUITier2Model;
+    [SerializeField] private GameObject upgradeUITier3Model;
+
+    private bool hasUpgradedOnce;
+    private bool hasUpgradedTwice;
+
     [SerializeField] private ParticleSystem upgradePS;
 
     private void Start()
     {
-        upgradeUI.SetActive(false);
-        upgradeUI.transform.GetChild(0).gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
+        upgradeUITier2.SetActive(false);
+        upgradeUITier2.transform.GetChild(0).gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
+
+        upgradeUITier3.SetActive(false);
+        upgradeUITier3.transform.GetChild(0).gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
     }
 
     public void ActivateUI(bool activate)
     {
-        upgradeUI.SetActive(activate);
+        if (!hasUpgradedOnce)
+            upgradeUITier2.SetActive(activate);
+        if(hasUpgradedOnce && !hasUpgradedTwice)
+            upgradeUITier3.SetActive(activate);
     }
 
     public void UpgradeDamageAdd(float upgrade, int cost)
@@ -28,8 +41,12 @@ public class TowerUpgradeHandler : MonoBehaviour
             Amount = upgrade,
             AttributeName = "DamageConfig/DamageCurve"
         };
+
         damageModifier.Apply(gameObject.GetComponent<ShootMonoBehaviour>().tower);
+        ApplyUpgradeFlag();
+        ApplyUpgradedModel();
     }
+
     public void UpgradeDamageMultiply(float upgrade, int cost)
     {
         if (StoreManager.Instance.CannotBuy(cost)) return;
@@ -41,6 +58,7 @@ public class TowerUpgradeHandler : MonoBehaviour
             AttributeName = "DamageConfig/DamageCurve"
         };
         damageModifier.Apply(gameObject.GetComponent<ShootMonoBehaviour>().tower);
+        ApplyUpgradeFlag();
     }
     public void UpgradeAOEDamageAdd(float upgrade, int cost)
     {
@@ -54,6 +72,7 @@ public class TowerUpgradeHandler : MonoBehaviour
         };
         damageModifier.Apply(gameObject.GetComponent<ShootMonoBehaviour>().tower);
         gameObject.GetComponent<ShootMonoBehaviour>().SetExplosiveDamage();
+        ApplyUpgradeFlag();
     }
     public void UpgradeAOERangeAdd(float upgrade, int cost)
     {
@@ -67,6 +86,7 @@ public class TowerUpgradeHandler : MonoBehaviour
         };
         rangeModifier.Apply(gameObject.GetComponent<ShootMonoBehaviour>().tower);
         gameObject.GetComponent<ShootMonoBehaviour>().SetExplosiveDamage();
+        ApplyUpgradeFlag();
     }
     public void UpgradeRangeAdd(float upgrade, int cost)
     {
@@ -79,6 +99,7 @@ public class TowerUpgradeHandler : MonoBehaviour
             AttributeName = "ProjectileConfig/Range"
         };
         rangeModifier.Apply(gameObject.GetComponent<ShootMonoBehaviour>().tower);
+        ApplyUpgradeFlag();
     }
     public void UpgradeFireRateSubtract(float upgrade, int cost)
     {
@@ -91,6 +112,7 @@ public class TowerUpgradeHandler : MonoBehaviour
             AttributeName = "ProjectileConfig/FireRate"
         };
         fireRateModifier.Apply(gameObject.GetComponent<ShootMonoBehaviour>().tower);
+        ApplyUpgradeFlag();
     }
     public void SellTower()
     {
@@ -98,4 +120,18 @@ public class TowerUpgradeHandler : MonoBehaviour
         StoreManager.Instance.Purchase(sellPrice);
         Destroy(gameObject);
     }
+
+    private void ApplyUpgradeFlag()
+    {
+        if (!hasUpgradedOnce)
+            hasUpgradedOnce = true;
+        if (hasUpgradedOnce && !hasUpgradedTwice)
+            hasUpgradedTwice = true;
+    }
+
+    private void ApplyUpgradedModel()
+    {
+
+    }
+
 }
