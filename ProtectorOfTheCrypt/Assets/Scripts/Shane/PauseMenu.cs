7bf217860e16a5f9721cad8f7327baa413622599
shanepearson62 <108286163+shanepearson62@users.noreply.github.com>
2023-11-07 19:15:44 -0500

    Pause Menu Update
    title

--- ProtectorOfTheCrypt/Assets/Scripts/Shane/PauseMenu.cs
+++ ProtectorOfTheCrypt/Assets/Scripts/Shane/PauseMenu.cs
@@ -33,22 +33,27 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
         PausePanel.SetActive(true);                                     //Pause Menu displays
-        //GameManager.instance.GamePaused(true);                          //Stops game
-        Time.timeScale = 0;
+        GameManager.instance.GamePaused(true);                          //Stops game
+        Time.timeScale = 0;                                             //Pauses Game Speed
         IGButtons.GetComponent<UIButtons>().UIButton.SetActive(false);  //Stops in game UI buttons disappear via UIButtons.cs
     }
 
     public void Continue()
     {
         PausePanel.SetActive(false);                                    //Pause menu goes away
-        //GameManager.instance.GamePaused(false);                       //Game resumes
-        Time.timeScale = 1;
+        GameManager.instance.GamePaused(false);                         //Game resumes
+        Time.timeScale = 1;                                             //Re-enables Game Speed
         IGButtons.GetComponent<UIButtons>().UIButton.SetActive(true);   //Makes in game UI buttons appear via UIButtons.cs
     }
 
     public void MainMenu()
     {
-        Time.timeScale = 1;
+        Time.timeScale = 1;                                             //Re-enables game speed
         SceneManager.LoadScene("MainMenuScene");                        //Load Main Menu Scene
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();                         //Completely Close Application
     }
 }
