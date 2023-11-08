a99fcb4f60fa8b8b10d356b0318a7c0a146ab37d
MaxJones2003 <jones.max2003@gmail.com>
2023-11-08 16:58:39 -0500

    scout enemy

--- ProtectorOfTheCrypt/Assets/FootStepCollision.cs
+++ ProtectorOfTheCrypt/Assets/FootStepCollision.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 public class FootStepCollision : MonoBehaviour
 {
     public ImpactType impactType;
+    [SerializeField] Transform foot1, foot2;
+    private float timer;
+    bool useFoot1 = true;
+
+    void Update()
+    {
+        // every 0.2 seconds call handle impact with the position of the footstep
+        timer += Time.deltaTime;
+        if (timer >= 0.2f)
+        {
+            timer = 0f;
+            Vector3 hitLocation = useFoot1 ? foot1.position : foot2.position;
+            Vector3 hitNormal = useFoot1 ? foot1.right : foot2.right;
+            Collider collider = useFoot1 ? foot1.GetComponent<Collider>() : foot2.GetComponent<Collider>();
+            useFoot1 = !useFoot1;
+            HandleImpact(hitLocation, hitNormal, collider);
+        }
+    }
 
     private void HandleImpact(Vector3 HitLocation, Vector3 HitNormal, Collider HitCollider)
     {
@@ -15,14 +33,5 @@ public class FootStepCollision : MonoBehaviour
             impactType,
             0
         );
-    }
-
-
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "Environment")
-        {
-            HandleImpact(other.GetContact(0).point, other.GetContact(0).normal, other.collider);
-        }
     }
 }
